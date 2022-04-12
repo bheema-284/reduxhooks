5520714f54e0a6b5efc95303058440b1941ae4f0
bheema-284 <bheemudug@gmail.com>
2022-04-12 17:41:10 +0530

    initial commit

---
+++ hooks/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
